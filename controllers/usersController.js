09bc1f220dcb1189538c2ca72f23908b429781a2
Ahmed <ahmed.massod@gmail.com>
2025-03-02 13:11:09 +0200

    Add/Delete/List items for the logged user and add/fix routes

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -9,3 +9,54 @@ export const profile = async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
+export const addItem = async (req, res) => {
+  try {
+    const userId = req.user; // get the current authenticated user
+    const newItem = req.body.item;
+
+    const user = await User.findById(userId);
+
+    // check if item already exists in the items list for the current user
+    if (user.items.includes(newItem)) {
+      return res
+        .status(400)
+        .json({ message: "Item already exists in the items list" });
+    }
+
+    await User.findByIdAndUpdate(userId, { $push: { items: newItem } });
+    res.status(200).json({ message: `${newItem} added successfully` });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const removeItem = async (req, res) => {
+  try {
+    const userId = req.user; // get current authenticated user
+    const itemToRemove = req.body.item;
+
+    await User.findByIdAndUpdate(userId, { $pull: { items: itemToRemove } });
+    res.status(200).json({ message: `${itemToRemove} removed successfully` });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const listItems = async (req, res) => {
+  try {
+    const userId = req.user; // get current authenticated user
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found!" });
+    }
+
+    res.status(200).json(user.items);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,17 @@
 import express from "express";
-import { profile } from "../controllers/usersController.js";
+import {
+  profile,
+  addItem,
+  removeItem,
+  listItems,
+} from "../controllers/usersController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/profile", authMiddleware, profile);
+router.get("/user", authMiddleware, profile);
+router.get("/user/items", authMiddleware, listItems);
+router.post("/user/items", authMiddleware, addItem);
+router.delete("/user/items", authMiddleware, removeItem);
 
 export default router;
