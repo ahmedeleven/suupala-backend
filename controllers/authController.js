3bcce1e6af22ae60bf3adb62020a703e0bc710a0
Ahmed <ahmed.massod@gmail.com>
2025-03-06 14:38:39 +0200

    Check token validity function

--- controllers/authController.js
+++ controllers/authController.js
@@ -68,3 +68,14 @@ export const loginUser = async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
+// check token validity
+export const checkTokenValidity = (req, res) => {
+  try {
+    const { token } = req.body;
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    res.status(200).json({ valid: true, token: decoded });
+  } catch (error) {
+    res.status(401).json({ valid: false, error });
+  }
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { registerUser, loginUser } from "../controllers/authController.js";
+import {
+  registerUser,
+  loginUser,
+  checkTokenValidity,
+} from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/checktoken", checkTokenValidity);
 
 export default router;
